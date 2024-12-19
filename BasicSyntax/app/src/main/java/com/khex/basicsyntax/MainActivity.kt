10a104675d36c55a2e13fb300955404c66ad8476
unknown <dugun319@gmail.com>
2024-12-20 00:48:17 +0900

    20241220

--- BasicSyntax/app/src/main/java/com/khex/basicsyntax/MainActivity.kt
+++ BasicSyntax/app/src/main/java/com/khex/basicsyntax/MainActivity.kt
@@ -13,14 +13,22 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        var myName  = "Smith"
+        val myName  = "Smith"
         var myAge   = 44
-        myAge       = myAge + 1
+        myAge       += 1
+
+        var out     = 0
+        val strike  = 3
+        if(strike < 2) {
+            out += 1
+        } else {
+            out = -10
+        }
 
 
 
 //        Log.d("BasicSyntax", "Print Out Log with Log.d ")
-        Log.d("BasicSyntax", "myName = $myName, myAge = $myAge")
+        Log.d("BasicSyntax", "myName = $myName & $myName(공백), myAge = $myAge, out = ${out+1}, out = $out+1")
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
